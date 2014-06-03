d86379c07b5bc0a1ec0b3f87ce61f35100b71def
Samuel Dillow <tarwich@gmail.com>
2014-06-03 14:24:06 -0500

    Update darkroom.js
    Added some events, and better bindings for hover/highlight.

--- darkroom.js
+++ darkroom.js
@@ -1,5 +1,8 @@
 
 (function($) {$(function() {
+	// --------------------------------------------------
+	// hover
+	// --------------------------------------------------
 	var hoverClasses = function(self) {
 		return $(self).find(".row_key").map(function(i,a) { 
 			return "hover_"+a.innerText.replace(/\W+/g, '-');
@@ -11,11 +14,27 @@
 	var unhighlight = function() {
 		$("#main").removeClass(hoverClasses(this));
 	};
-	$(".button:has(.row_key)").hover(highlight, unhighlight);
+	// Need interval in order ot update new items
+	$("#main")
+		.on("mouseenter", ".button:has(.row_key)", highlight)
+		.on("mouseleave", ".button:has(.row_key)", unhighlight)
+	;
 
 	var style = $("#stores .storeRow").map(function(i,a) { 
 		return "#main."+a.id.replace(/^row/, "hover") + " #"+a.id; 
 	}).toArray().join(", ")+" { background-color: #fee; }";
 	$("head").append("<style type='text/css'>"+style+"</style>")
-	
+
+	// --------------------------------------------------
+	// click buttons
+	// --------------------------------------------------
+	if(window["buttonInterval"]) window.clearInterval(window.buttonInterval);
+	window.buttonInterval = window.setInterval(function() {
+		$("#trapsButton,#gatherButton,#build_trap").not(".disabled").click();
+		// - events -----------------------------------------
+		// Ruined traps
+		$("#event:has(.eventTitle:contains(A Ruined Trap)) #track,#end").click()
+		// Noises
+		$("#event:has(.eventTitle:contains(Noises)) #ignore").click()
+	}, 350);
 });})(jQuery);
