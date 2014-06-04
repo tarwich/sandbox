ddcb7720537d016a2b5ee2638bffb912adc48e19
Samuel Dillow <tarwich@gmail.com>
2014-06-03 23:43:07 -0500

    Update topa.js
    Testing ready state

--- topa.js
+++ topa.js
@@ -1,2 +1,29 @@
+(function(NS) {
+	// Initialize the namespace
+	var ns = window[NS] || (window[NS] = {});
+	
+	try { document.addEventListener("DOMContentLoaded", ns.ready); }
+	catch(ex) {
+		document.attachEvent("onreadystatechange", ns.readyStateChange);
+	}
+	
+	ns.ready = function() {
+		console.log("READY"); 
+	};
+	
+	ns.readyStateChange = function() {
+		if(document.readyState == "complete") {
+			document.detachEvent("onreadystatechange", ns.readyStateChange);
+			ns.ready();
+		}
+	};
+})("topa-extensions");
 
-console.log("Yay!");
+// (function(callback) {
+// })
+// (function() {
+//   location.search.replace(/\??(.*?)=(.*?)(?:&|$)/g, function(a,title,newValue) {
+//     var nodes = document.querySelectorAll("[title='"+title+"'");
+//     for(var i=0, iMax=nodes.length; i<iMax; ++i) nodes[i].value = unescape(newValue);
+//   })
+// });</script>​​​​​​​​​​​​​​
