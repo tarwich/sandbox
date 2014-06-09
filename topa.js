f546a4d101b842b0354634d8adb168370680c75b
Samuel Dillow <tarwich@gmail.com>
2014-06-09 01:11:13 -0500

    Update topa.js
    Got the parent to call the child form correctly. Need refresh.

--- topa.js
+++ topa.js
@@ -1,33 +1,104 @@
-(function(NS) {
+(function(NS, init) {
 	// Initialize the namespace
-	var ns = window[NS] || (window[NS] = {});
+	ns = window[NS] || (window[NS] = {});
 	
+	// --------------------------------------------------
+	// initialize
+	// --------------------------------------------------
 	ns.initialize = function() {
-		try { document.addEventListener("DOMContentLoaded", ns.ready); }
-		catch(ex) {
-			document.attachEvent("onreadystatechange", ns.readyStateChange);
-		}
+		// Add jQuery to the document
+		if(!jQuery) document.head.appendChild(document.createElement("script")).src = "//ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js";
+		
+		// Wait for jQuery to be available
+		ns.waitFor(function() { return window.jQuery; }).then(function($) {
+			// Create a document.ready function
+			$(function() {
+				// Activate all a[function] nodes
+				$("a[function]").each(function() {
+					var $node = $(this);
+					// Make sure the node has a pound link for IE8
+					$node.attr("href", "#");
+					// Change the function from foo to function_foo
+					functionName = ["function_", $node.attr("function")].join("");
+					// Lookup (and call) the function in our namespace
+					$node.click((ns[functionName] || function() { 
+						console.warn(["Function '",functionName,"' not known"].join("")); 
+					}));
+				});
+			});
+		});
 	};
 	
-	ns.ready = function() {
-		console.log("READY"); 
+	// --------------------------------------------------
+	// function_add_child
+	// --------------------------------------------------
+	ns.function_add_child = function() {
+		var $this = $(this);
+		
+		// Get data from the URI
+		$this.prop("baseURI").replace(/\?(.*?)=(.*?)(&|$)/g, function(a,b,c) {$this[b] = c; });
+		// Get the base url
+		var baseURL = $this.prop("baseURI").match(/.*?\/Lists(?=\/)/)[0];
+		var url = [baseURL,"/",$this.attr("list"),"/NewForm.aspx?",$this.attr("child_column"),"=",$this.ID].join("");
+		
+		// Wait for the iFrame to show up
+		ns.waitFor("iframe.ms-dlgFrame").then(function(node) {
+			$(node).load(function() {
+				var $document = $(this.contentDocument);
+				// Process all the URI information for the child document
+				$document.prop("baseURI").replace(/\?(.*?)=(.*?)(&|$)/g, function(a, b, c) {
+					// Set the value of this input element
+					$document.find("[title="+b+"]").val(c)
+					// Hide the input field
+					.closest("tr").hide();
+				});
+			});
+		});
+		
+		SP.UI.ModalDialog.showModalDialog({
+			url  : url,
+			title: $this.attr("caption") || "Enter child information",
+		});
+		
 	};
-	
-	ns.readyStateChange = function() {
-		if(document.readyState == "complete") {
-			document.detachEvent("onreadystatechange", ns.readyStateChange);
-			ns.ready();
-		}
+		
+	// --------------------------------------------------
+	// waitFor
+	// --------------------------------------------------
+	// Returns a Promise of sorts when the selector is 
+	// found. Selector should not be a jQuery object, 
+	// because since 1.7 it's impossible to rer[]un a 
+	// jQuery selector.
+	ns.waitFor = function(selector) {
+		var promise; promise = {
+			callbacks: [],
+			then: function(callback) { 
+				this.callbacks.push(callback); 
+				return this; 
+			},
+			resolve: function(payload) {
+				for(var i=0, iMax=this.callbacks.length; i<iMax; ++i)
+					this.callbacks[i](payload);
+			},
+		};
+		
+		// If the selector is a function, then use that function as the test
+		if(typeof(selector) == "function") test = selector;
+		// If the selector isn't a function, then use a function that tests a jQuery selector
+		else test = function() { return jQuery(selector); };
+		
+		window.setTimeout(function tick() {
+			var result = test();
+			if(result) {
+				// If this is a jQuery object, then make sure there are results
+				if(result.jquery) { if(result.length) return promise.resolve(result);
+				// Otherwise, a non-falsy value means success 
+				} else return promise.resolve(result);
+			}
+			window.setTimeout(tick, 500);
+		}, 1);
+		return promise;
 	};
 	
 	ns.initialize();
 })("topa-extensions");
-
-// (function(callback) {
-// })
-// (function() {
-//   location.search.replace(/\??(.*?)=(.*?)(?:&|$)/g, function(a,title,newValue) {
-//     var nodes = document.querySelectorAll("[title='"+title+"'");
-//     for(var i=0, iMax=nodes.length; i<iMax; ++i) nodes[i].value = unescape(newValue);
-//   })
-// });</script>​​​​​​​​​​​​​​
